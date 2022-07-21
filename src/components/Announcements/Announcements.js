745070c1d72f3068c840ec55ec71c4314fd9a7f6
jonyeokj <joyekajie@gmail.com>
2022-07-22 04:08:02 +0800

    Announcements Function

--- src/components/Announcements/Announcements.js
+++ src/components/Announcements/Announcements.js
@@ -1,14 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import "./Announcements.css";
 import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../firebase";
+import { query, collection, getDocs, where } from "firebase/firestore";
+import Collapsible from "react-collapsible";
 
 const Announcements = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const [tempMods, setTempMods] = useState([]);
+  const [announcements, setAnnouncements] = useState([]);
+  const [mods, setModules] = useState([]);
+  const [dataLoaded, setDataLoaded] = useState(false);
   const navigate = useNavigate();
+
+  const fetchModules = async () => {
+    try {
+      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const data = doc.docs[0].data();
+
+      setTempMods(data.modules);
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+
+  const fetchAnnouncements = async () => {
+    try {
+      const q = query(collection(db, "modules"), where("code", "in", tempMods));
+      const doc = await getDocs(q);
+      const modList = doc.docs.map((code) => code.data().code);
+      const announcementList = doc.docs.map(
+        (announcements) => announcements.data().announcements
+      );
+
+      console.log(modList, announcementList);
+
+      setModules(modList);
+      console.log("ann", mods);
+      setAnnouncements(announcementList);
+      setDataLoaded(true);
+    } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+    }
+  };
+
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/Pigeon");
+    fetchModules();
+  }, [user, loading]);
+
+  useEffect(() => {
+    if (tempMods.length > 0) {
+      fetchAnnouncements();
+    }
+  }, [tempMods]);
 
   return (
     <div className="text">
-      <div>Announcements</div>
-      <button onClick={() => navigate("/Pigeon/Dashboard")}> Back</button>
+      {dataLoaded ? (
+        <>
+          <div>Announcements</div>
+          {mods.map((mod, idx) => (
+            <Collapsible trigger={mod}>
+              {Object.keys(announcements[idx]).map((key) => (
+                <div>
+                  <h3>{`${key}`}</h3>
+                  <div>{`${announcements[idx][key]}`}</div>
+                </div>
+              ))}
+            </Collapsible>
+          ))}
+          <button onClick={() => navigate("/Pigeon/Dashboard")}> Back</button>
+        </>
+      ) : (
+        <div>Loading</div>
+      )}
     </div>
   );
 };
--- src/components/Coocoo/Coocoo.js
+++ src/components/Coocoo/Coocoo.js
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from "react";
 import "./Coocoo.css";
-import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../../firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import Collapsible from "react-collapsible";
-import { render } from "@testing-library/react";
 
 const Coocoo = () => {
   const [user, loading, error] = useAuthState(auth);
-  const [modules, setModules] = useState([]);
+  const [tempMods, setTempMods] = useState([]);
   const [links, setLinks] = useState([]);
-  const [displayedMods, setMWL] = useState([]);
+  const [mods, setModules] = useState([]);
   const navigate = useNavigate();
 
   const fetchModules = async () => {
@@ -20,9 +18,8 @@ const Coocoo = () => {
       const q = query(collection(db, "users"), where("uid", "==", user?.uid));
       const doc = await getDocs(q);
       const data = doc.docs[0].data();
-      console.log(data);
 
-      setModules(data.modules);
+      setTempMods(data.modules);
     } catch (err) {
       console.error(err);
       alert("An error occured while fetching user data");
@@ -31,14 +28,13 @@ const Coocoo = () => {
 
   const fetchLinks = async () => {
     try {
-      const q = query(collection(db, "modules"), where("code", "in", modules));
+      const q = query(collection(db, "modules"), where("code", "in", tempMods));
       const doc = await getDocs(q);
       const modList = doc.docs.map((code) => code.data().code);
       const linkList = doc.docs.map((url) => url.data().url);
 
       setLinks(linkList);
-      console.log(Object.values(linkList));
-      setMWL(modList);
+      setModules(modList);
     } catch (err) {
       console.error(err);
       alert("An error occured while fetching user data");
@@ -52,20 +48,15 @@ const Coocoo = () => {
   }, [user, loading]);
 
   useEffect(() => {
-    if (modules.length > 0) {
+    if (tempMods.length > 0) {
       fetchLinks();
     }
-  }, [modules]);
-
-  function displayedList(displayedMods, links) {
-    return <div></div>;
-  }
+  }, [tempMods]);
 
   return (
     <div className="text">
       <div>Coocoo</div>
-      {/* {displayedList(displayedMods)} */}
-      {displayedMods.map((mod, idx) => (
+      {mods.map((mod, idx) => (
         <Collapsible trigger={mod}>
           {Object.keys(links[idx]).map((key) => (
             <div>
